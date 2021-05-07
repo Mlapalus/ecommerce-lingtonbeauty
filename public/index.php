1ad346fcd4098d964977e38f64ccd3c94eed35e5
mlapalus <mlapalus@yahoo.fr>
2021-05-07 19:21:40 +0200

    add bootstrap.php config file

--- public/index.php
+++ public/index.php
@@ -5,9 +5,7 @@ use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/vendor/autoload.php';
-
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+require dirname(__DIR__) . '/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
