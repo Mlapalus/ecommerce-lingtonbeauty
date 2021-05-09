38a17c372b19e38a3a15f111cad87286143aa7c9
mlapalus <mlapalus@yahoo.fr>
2021-05-09 16:33:42 +0200

    add products to database

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use App\Entity\Product;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -28,19 +29,27 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToUrl('Retour sur le site', 'fa fa-home', '/');
-        yield MenuItem::linkToUrl('Retour dans la boutique', 'fa fa-home', '/shop');
-
         yield MenuItem::linktoDashboard('Tableau de Bord', 'fa fa-home');
 
-        yield MenuItem::section('Utilisateurs');
+        yield MenuItem::section('       ');
+        yield MenuItem::linkToUrl('Retour sur le site', 'fa fa-blog', '/');
+        yield MenuItem::linkToUrl('Retour dans la boutique', 'fa fa-store', '/shop');
+
+        yield MenuItem::section('       ');
+        yield MenuItem::subMenu('Utilisateurs');
         yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class);
-        yield MenuItem::linkToCrud('Ajouter un Utilisateur', 'fas fa-user', User::class)
+        yield MenuItem::linkToCrud('Ajouter un Utilisateur', 'fas fa-user-plus', User::class)
             ->setAction('new');
 
+        yield MenuItem::section('       ');
         yield MenuItem::section('Produits');
+        yield MenuItem::subMenu('Produits');
+        yield MenuItem::linkToCrud('Produits', 'fas fa-barcode', Product::class);
+        yield MenuItem::linkToCrud('Ajouter un produit', 'fas fa-cart-plus', Category::class)
+            ->setAction('new');
+        yield MenuItem::subMenu('Catégories');
         yield MenuItem::linkToCrud('Catégories', 'fas fa-list', Category::class);
-        yield MenuItem::linkToCrud('Ajouter une catégorie', 'fas fa-list', Category::class)
+        yield MenuItem::linkToCrud('Ajouter une catégorie', 'fas fa-cart-plus', Category::class)
             ->setAction('new');
     }
 }
