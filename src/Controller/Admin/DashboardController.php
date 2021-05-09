fe5d26e0e916697dd315c37ce694df33f9d42c87
mlapalus <mlapalus@yahoo.fr>
2021-05-09 02:30:52 +0200

    add Category and Product entity

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -27,7 +28,19 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
+        yield MenuItem::linkToUrl('Retour sur le site', 'fa fa-home', '/');
+        yield MenuItem::linkToUrl('Retour dans la boutique', 'fa fa-home', '/shop');
+
         yield MenuItem::linktoDashboard('Tableau de Bord', 'fa fa-home');
-        yield MenuItem::linkToCrud('Utilisateur', 'fas fa-user', User::class);
+
+        yield MenuItem::section('Utilisateurs');
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class);
+        yield MenuItem::linkToCrud('Ajouter un Utilisateur', 'fas fa-user', User::class)
+            ->setAction('new');
+
+        yield MenuItem::section('Produits');
+        yield MenuItem::linkToCrud('Catégories', 'fas fa-list', Category::class);
+        yield MenuItem::linkToCrud('Ajouter une catégorie', 'fas fa-list', Category::class)
+            ->setAction('new');
     }
 }
